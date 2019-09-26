468cddc9c9a10b5c1890a60d0c5b6bfc698c9a8d
Senthuran Elangovan <senthuran@pikanite.com>
2019-09-26 15:07:05 +0530

    Init

--- README.md
+++ README.md
@@ -1,29 +1,6 @@
 # vue-date-range-picker
 
-## Project setup
+## Date Range Picker Modified
 ```
-npm install
+[vue-hotel-datepicker](https://github.com/krystalcampioni/vue-hotel-datepicker)
 ```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="text-align: left; padding: 10vw">
+    <div>
+      <div class="box">
+        <h3>Week</h3>
+        <DatePicker
+          :minNights="7"
+          :allowedRanges="[7,14,21,28,35]"
+          @check-in-changed="checkInChanged"
+          @check-out-changed="checkOutChanged"
+        />
+      </div>
+      <div class="box">
+        <h3>Month</h3>
+        <DatePicker :minNights="30" :allowedRanges="[30,60,90,120,150]" />
+      </div>
+      <div class="box">
+        <h3>Reset From out side</h3>
+        <button @click="resetFn()">Reset</button>
+        <DatePicker :isReset="isReset" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DatePicker from "@/components/DatePicker.vue";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    DatePicker
+  },
+
+  data() {
+    return {
+      isReset: null,
+      rangeType: "week",
+      ptPT: {
+        night: "Noite",
+        nights: "Noites",
+        "day-names": ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+        "check-in": "Chegada",
+        "check-out": "Partida",
+        "month-names": [
+          "Janeiro",
+          "Fevereiro",
+          "Março",
+          "Abril",
+          "Maio",
+          "Junho",
+          "Julho",
+          "Agosto",
+          "Setembro",
+          "Outubro",
+          "Novembro",
+          "Dezembro"
+        ]
+      }
+    };
+  },
+  methods: {
+    checkInChanged(val) {
+      console.log(val);
+    },
+    checkOutChanged(val) {
+      console.log(val);
+    },
+    resetFn() {
+      this.isReset = Number(new Date());
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,
+html {
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+.box {
+  width: 100%;
 }
 </style>
