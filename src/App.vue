ad2aaa4ccdf1e7f28bc4594c0c60f677eac366da
Senthuran Elangovan <senthuran@pikanite.com>
2019-10-08 11:40:12 +0530

    Example created

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,78 @@
 <template>
   <div id="app" style="text-align: left; padding: 10vw">
-    <div>
-      <div class="box">
-        <DatePicker  />
+    <div class="box">
+      <div class="columns">
+        <div class="column">
+          <code>
+            <h3>Mode : "Single" , Multi , Range</h3>
+          </code>
+        </div>
+        <div class="column">
+          <div class="field">
+            <div class="control">
+              <div class="select is-primary">
+                <select v-model="selcetedType">
+                  <option v-for="type in types" :key="type">{{type}}</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <DatePicker :mode="selcetedType" />
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="columns">
+        <div class="column">
+          <code>
+            <h3>Assign startDate and endDate</h3>
+          </code>
+        </div>
+        <div class="column">
+          <div class="field">
+            <div class="control">
+              <div class="select is-primary">
+                <select v-model="selcetedType">
+                  <option v-for="type in types" :key="type">{{type}}</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <DatePicker :mode="selcetedType" :startDate="startDate" :endDate="endDate" />
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="columns">
+        <div class="column">
+          <code>
+            <h3>Assign Disable Dates</h3>
+          </code>
+        </div>
+        <div class="column">
+          <div class="field">
+            <div class="control">
+              <div class="select is-primary">
+                <select v-model="selcetedType">
+                  <option v-for="type in types" :key="type">{{type}}</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column"> 
+          <DatePicker :mode="selcetedType" :disabledDates="disabledDates" />
+        </div>
       </div>
     </div>
   </div>
@@ -18,31 +88,18 @@ export default {
 
   data() {
     return {
-      isOldDayDisable: false,
-      name: null,
-      isReset: null,
-      rangeType: "week",
-      ptPT: {
-        night: "Noite",
-        nights: "Noites",
-        "day-names": ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
-        "check-in": "Chegada",
-        "check-out": "Partida",
-        "month-names": [
-          "Janeiro",
-          "Fevereiro",
-          "Março",
-          "Abril",
-          "Maio",
-          "Junho",
-          "Julho",
-          "Agosto",
-          "Setembro",
-          "Outubro",
-          "Novembro",
-          "Dezembro"
-        ]
-      }
+      //example
+      types: ["Single", "Multi", "Range"],
+      selcetedType: "Single",
+
+      endDate: new Date(2019, 11, 15),
+      startDate: new Date(2019, 9, 3),
+      disabledDates: [
+        new Date(2019, 9, 25),
+        new Date(2019, 9, 22),
+        new Date(2019, 9, 14),
+        new Date(2019, 9, 24)
+      ]
     };
   },
   methods: {
